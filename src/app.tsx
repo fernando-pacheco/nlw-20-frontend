74624227f6376055cf53bf08ea3c89bf05e80a2d
Fernando Pacheco <fernandopachecopx@gmail.com>
2025-07-09 21:35:43 -0300

    chore: primeiras configurações

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div>
+            <h1 className="text-6xl font-bold text-blue-500">Hello World!</h1>
+            <p className="mt-3 text-2xl">
+                Start editing to see some magic happen!
+            </p>
+        </div>
+    )
+}
+
+export { App }
